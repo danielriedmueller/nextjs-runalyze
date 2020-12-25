192da05c58086e84f01f6008019f1956e2e214c6
danielriedmueller <daniel.riedmuller@gmail.com>
2020-12-25 10:40:52 +0100

    cors

--- pages/api/all.js
+++ pages/api/all.js
@@ -1,6 +1,20 @@
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
 const db = require('better-sqlite3')(process.env.DATABASE_URL);
 
+// Initialize the cors middleware
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // Only allow requests with GET, POST and OPTIONS
+        methods: ['GET', 'POST', 'OPTIONS'],
+    })
+)
+
 export default async function handle(req, res) {
+    // Run cors
+    await cors(req, res);
+
     const rows = db.prepare('SELECT * FROM runs ORDER BY date desc').all();
     res.json(rows);
 }
--- pages/api/upsert.js
+++ pages/api/upsert.js
@@ -1,6 +1,21 @@
+import initMiddleware from "../../lib/init-middleware";
+import Cors from "cors";
+
 const db = require('better-sqlite3')(process.env.DATABASE_URL);
 
+// Initialize the cors middleware
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // Only allow requests with GET, POST and OPTIONS
+        methods: ['GET', 'POST', 'OPTIONS'],
+    })
+)
+
 export default async function handle(req, res) {
+    // Run cors
+    await cors(req, res);
+
     let {date, distance, duration, id} = req.body;
 
     if (id === 0) {
