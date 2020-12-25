8a68e2004d1ea0a23973181302ddca8e9c5bb2b3
danielriedmueller <daniel.riedmuller@gmail.com>
2020-12-25 22:11:35 +0100

    pwa

--- next.config.js
+++ next.config.js
@@ -5,19 +5,5 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
     pwa: {
         dest: 'public'
-    },
-    webpack: (config, { isServer }) => {
-        config.plugins.push(
-            new CopyWebpackPlugin(
-                {
-                    patterns: [
-                        { from: path.join(__dirname, 'assets/pwa'), to: path.join(__dirname, 'public/') },
-                        { from: path.join(__dirname, 'assets/manifest.json'), to: path.join(__dirname, 'public/manifest.json') },
-                        { from: path.join(__dirname, 'assets/favicon.ico'), to: path.join(__dirname, 'public/favicon.ico') }
-                    ]
-                }
-            )
-        );
-        return config;
     }
 })
