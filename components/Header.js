7ae97de0df442728c7611ae1819515b0b563e360
danielriedmueller <daniel.riedmuller@gmail.com>
2020-12-26 00:50:35 +0100

    no data, no error

--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,7 @@ export default class Header extends Component {
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="theme-color" content="#e63946" />
                 <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-                <title>Next.js PWA Example</title>
+                <title>Runaylze</title>
                 <link rel='manifest' href='/manifest.json' />
             </Head>
             <header className={style.header}><h1>Runalyze</h1></header>
