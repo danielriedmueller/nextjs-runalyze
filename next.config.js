42307516d4b4ed077225f575cdcbf07b79d5fa3c
danielriedmueller <daniel.riedmuller@gmail.com>
2020-12-25 20:21:33 +0100

    pwa

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = withPWA({
             new CopyWebpackPlugin(
                 {
                     patterns: [
-                        { from: path.join(__dirname, 'assets/pwa'), to: path.join(__dirname, 'public/icons') },
+                        { from: path.join(__dirname, 'assets/pwa'), to: path.join(__dirname, 'public') },
                         { from: path.join(__dirname, 'assets/manifest.json'), to: path.join(__dirname, 'public/manifest.json') },
                         { from: path.join(__dirname, 'assets/favicon.ico'), to: path.join(__dirname, 'public/favicon.ico') }
                     ]
