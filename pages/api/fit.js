7801d06227bb8380f4a208e20e2ab2c62bc6d8b8
danielriedmueller <daniel.riedmuller@gmail.com>
2022-04-18 17:57:50 +0200

    google fit

--- pages/api/fit.js
+++ pages/api/fit.js
@@ -1,7 +1,14 @@
 // Initialize the cors middleware
 import initMiddleware from "../../lib/init-middleware";
 import Cors from "cors";
-const FormData = require('form-data');
+import dayjs from "dayjs";
+import {jsonToRun} from "../../helper/functions";
+
+const fitnessDataTypes = {
+    'distance': 'com.google.distance.delta',
+    'steps': 'com.google.step_count.delta',
+    'calories': 'com.google.calories.expended'
+}
 
 const cors = initMiddleware(
     // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
@@ -17,14 +24,6 @@ export default async function handle(req, res) {
 
     const {token} = req.body;
 
-    /*
-    let formData = new FormData();
-    formData.append('distance', '5');
-    formData.append('unit', 'km');
-    formData.append('time', '01:00:00');
-
-     */
-
     const startTime = '2022-04-11T00:00:00.000Z';
     const joggingActivityType = 56;
     const params = new URLSearchParams({
@@ -32,14 +31,71 @@ export default async function handle(req, res) {
         startTime: startTime,
     });
 
-    const fitData = await fetch('https://fitness.googleapis.com/fitness/v1/users/me/sessions?' + params, {
+    let runs = [];
+
+    let foo = await fetch('https://fitness.googleapis.com/fitness/v1/users/me/sessions?' + params, {
         headers: {
             'Content-type': 'application/json',
-            'Authorization': `Bearer ${token}`, // notice the Bearer before your token
+            'Authorization': `Bearer ${token}`
         },
+    }).then(response => response.json()).then(data => {
+        data.session.forEach(async (session) => {
+            const run = await fetch('https://fitness.googleapis.com/fitness/v1/users/me/dataset:aggregate', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    "startTimeMillis": session.startTimeMillis,
+                    "endTimeMillis": session.endTimeMillis,
+                    "aggregateBy": Object.values(fitnessDataTypes).map(val => {
+                        return {'dataTypeName': val};
+                    }),
+                    "bucketBySession": {}
+                })
+            }).then(response => response.json()).then(data => createRun(data.bucket[0]));
+        })
+    });
+    console.log(runs);
+
+    res.json(runs);
+}
+
+function createRun(data) {
+    const fitnessData = getFitnessDataFromDataset(data.dataset);
+    const date = new Date(parseInt(data.session.startTimeMillis));
+
+    return jsonToRun({
+        date,
+        distance: fitnessData['distance'],
+        duration: '11000',
+        vdot: 0,
+        id: 0
+    });
+}
+
+function getFitnessDataFromDataset(datasets) {
+    let fitnessData = [];
+
+    datasets.forEach((dataset) => {
+        dataset.point.forEach((point) => {
+            Object.entries(fitnessDataTypes).forEach(([type, apiName]) => {
+                if (point.dataTypeName === apiName) {
+                    if (!Array.isArray(fitnessData[type])) {
+                        fitnessData[type] = [];
+                    }
+
+                    // Get always first value only, see Google API Response documentation
+                    fitnessData[type].push(...point.value.map(valObj => valObj[Object.keys(valObj)[0]]));
+                }
+            })
+        })
     });
 
-    const fitRuns = await fitData.json();
+    for (const type in fitnessData) {
+        fitnessData[type] = fitnessData[type].reduce((a, b) => a + b);
+    }
 
-    res.json(fitRuns);
+    return fitnessData;
 }
